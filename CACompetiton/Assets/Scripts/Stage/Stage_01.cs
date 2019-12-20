c5176597f32505ce6cf6e8e9daf7f4b532f6c0bc
okayamakodai <libra@libranomac.local>
2019-12-20 13:32:08 +0900

    unityファイル変更

---
+++ CACompetiton/Assets/Scripts/Stage/Stage_01.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage_01 : MonoBehaviour
+{
+    private GameObject Enemy_01;
+    private GameObject Enemy_02;
+    private long time_count = 0;
+    [SerializeField] private GameObject Canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy_01 = (GameObject)Resources.Load("Enemy_01");
+        Enemy_02 = (GameObject)Resources.Load("Enemy_02");
+        StartCoroutine(Canvas.GetComponent<Stage_Title>().title("Stage01"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (time_count)
+        {
+            case 0:
+                GameObject new_obj = Instantiate(Enemy_01, new Vector3(12, 4, 0.0f), Quaternion.identity);
+                new_obj.GetComponent<Enemy_Status>().Init(10, 0);
+
+                new_obj = Instantiate(Enemy_01, new Vector3(13, 4, 0.0f), Quaternion.identity);
+                new_obj.GetComponent<Enemy_Status>().Init(10, 0);
+                new_obj = Instantiate(Enemy_01, new Vector3(14, 4, 0.0f), Quaternion.identity);
+                new_obj.GetComponent<Enemy_Status>().Init(10, 0);
+                break;
+            case 100:
+                break;
+        }
+        time_count += 1;
+    }
+}
---
+++ CACompetiton/Assets/Scripts/UI/Stage_Title.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stage_Title : MonoBehaviour
+{
+    private string output_str = "";
+    [SerializeField] private GameObject stage_title;
+
+    private IEnumerator title_del(string str)
+    {
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                output_str += str[j];
+                stage_title.GetComponent<Text>().text = output_str;
+            }
+            output_str = "";
+            yield return new WaitForSeconds(0.03f);
+            output_str = "";
+            stage_title.GetComponent<Text>().text = output_str;
+        }
+        yield break;
+    }
+    public IEnumerator title(string str)
+    {
+        foreach (char c in str)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string temp_str = output_str + (char)Random.Range(33, 126);
+                stage_title.GetComponent<Text>().text = temp_str;
+                yield return new WaitForSeconds(0.01f);
+            }
+            output_str += c;
+            stage_title.GetComponent<Text>().text = output_str;
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(1f);
+        output_str = "";
+        StartCoroutine(title_del(str));
+        yield break;
+    }
+
+
+}
