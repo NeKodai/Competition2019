0f3c0b2a273d1fe7f40da45d3b386dde1c16055c
okayamakodai <libra@libranomac.local>
2019-12-28 22:49:35 +0900

    graphics追加

---
+++ CACompetition/Assets/Scripts/Stages/Stage1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1 : MonoBehaviour
+{
+    private GameObject Enemy;
+    private int flame_count = 0;
+    [SerializeField] GameObject Enemy_Field;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = (GameObject)Resources.Load("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (flame_count)
+        {
+            case 0:
+                break;
+        }
+        flame_count += 1;
+    }
+}
